85d433137f70123b1ef09fd6807b060db03c4cdc
吴铭 <451517996@qq.com>
2026-02-23 20:27:19 +0800

    use ice as base rpc

--- lgbf/common/Class1.cs
+++
@@ -1,5 +0,0 @@
-﻿namespace common;
-
-public class Class1
-{
-}
---
+++ lgbf/common/IceRPC.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.IO;
+using System.Collections;
+using System.Collections.Generic;
+using Grid;
+
+namespace common;
+
+public partial interface IGridService
+{
+    
+}
