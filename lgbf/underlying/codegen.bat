5c6560a05726852a2f7410ae4b9e0c216b68ff43
qianqians <451517996@qq.com>
2026-03-17 16:15:45 +0800

    underlying client

--- lgbf/underlying/codegen.bat
+++ lgbf/underlying/codegen.bat
@@ -1,3 +1,4 @@
 cd ../../tools
 
 protoc --csharp_out=../lgbf/hub  --proto_path=../lgbf/underlying  ../lgbf/underlying/underlying.proto
+protoc --csharp_out=../gem/unity/Assets/Script/ServerSDK  --proto_path=../lgbf/underlying  ../lgbf/underlying/underlying.proto
