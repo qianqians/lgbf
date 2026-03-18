f455d3aa9ae68c919cabc502435f4ae83cf2a0d2
qianqians <451517996@qq.com>
2026-03-18 15:30:03 +0800

    EntityTokenConvertGuidKey

--- lgbf/hub/RedisHelp.cs
+++ lgbf/hub/RedisHelp.cs
@@ -5,6 +5,8 @@ public static class RedisHelp
 {
     public static readonly string EntityLockKey = "EntityLock:{0}";
     
+    public static readonly string EntityTokenConvertGuidKey = "EntityTokenGuid:{0}";
+    
     public static readonly string EntityStoreKey = "EntityStore{0}:{1}";
 
     public static readonly string EntityStoreMongodbList = "EntityStoreMongodbList";
